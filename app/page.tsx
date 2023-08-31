9c9420db794229503665ad4e4e86669d7c53057e
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-08-31 14:41:02 +0700

    feat: create ui login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import Image from 'next/image'
+'use client'
+
+import {
+  Input,
+  Button
+} from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Test</h1>
-    </main>
+    <>
+      <div className='w-full h-full flex items-center justify-center'>
+        <div className='w-6/12'>
+          <h1 className='mb-10 text-4xl font-bold text-center text-success'>
+            NextJs Basic
+          </h1>
+          <h1 className='mb-5 font-medium'>
+            Login
+          </h1>
+          <Input
+            label='Email'
+            type='email'
+            isRequired
+            className='mb-5'
+          />
+          <Input
+            label='Password'
+            type='password'
+            isRequired
+            className='mb-5'
+          />
+          <Button
+            size='lg'
+            color='success'
+            className='w-full'
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    </>
   )
 }
